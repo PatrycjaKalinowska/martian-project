973439e909076e1ef9209e23cb3e81cd9a0ca9ff
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-04-27 15:12:18 +0200

    adding_basing_styling_for_menu_bar

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 const Header = () => {
 
     return <header className="header">
-            <div className="logo">LogoMartianProject</div>
+            <div className="logo">Logo-Martian-Project</div>
             <ul className="menu">
             <li className="menu-item">
                 <a href="/"> weather </a>
