3bb082619ca89840f80f74399d41db82be7503f9
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-04-27 15:53:31 +0200

    adding basic styling for footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
 
     return <footer className="footer">
             <ul className="footer-menu">
-                <li className="footer-menu-logo">LogoMartianProject</li>
                 <li className="footer-menu-rights">2022 <span>Martian Project &copy;</span> All rights reserved.</li>
             </ul>
         </footer>
