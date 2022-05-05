f9e9fc2aa9430beb120a44088af089bf84f4a794
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-05-05 15:50:25 +0200

    fixed_initial_styling_in_Article_component

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -6,6 +6,10 @@ const Gallery = () => {
         <h2 className="gallery-page-title">Gallery</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <div className="gallery-container">
+
+        <div className="gallery-card">
+            <img src="https://source.unsplash.com/random" alt="" className="gallery-card-photo"/>
+        </div>
         
         </div>
     
