bf8d1b5b8b4fb7657ef49eab9cf016214a6c8a65
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-05-05 12:10:40 +0200

    Weather_component_fetching_data_draft

--- src/Weather.js
+++ src/Weather.js
@@ -3,41 +3,29 @@ import WeatherBox from "./WeatherBox";
 import './Weather.css';
 
 const Weather = () => {
-    const [weather, setWeather] = useState();
-    // const [solKeys, setSolKeys] = useState();
-    // const [isLoadingWeather, setIsLoadingWeather] = useState('true');
+    const [initialData, setInitialData] = useState();
 
     useEffect(() => {
         fetch('https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0')
         .then(res => res.json()).then(resData => {
-            return setWeather(() => resData); 
+            return setInitialData(() => resData); 
         });
     }, []);
-
-    console.log(weather);
     
-    if (!weather) {
+    if (!initialData) {
         return <div className="data-loading-screen">Data is loading... ... ...</div>
     } else {
-        // const data = [];
-        // for (let key in weather.validity_checks) {
-        //     data.push(weather.validity_checks[key]);
-        // }
-        let data = Object.entries(weather.validity_checks);
-
-        console.log(data);
+        const data = Object.entries(initialData.validity_checks).filter((element, index) => index<6)
+        console.log(initialData);
         return (
             <div className="weather-content">
                <h2 className="weather-header">Latest weather on Mars</h2>
                <ul>
-                { weather.validity_checks.sols_checked.map( (item) => <li key={item}>
-                <WeatherBox weather={weather}>{item}</WeatherBox>
-                </li>) }
+                { data.map(item => <li key={item[0]}><WeatherBox item={item}/></li>) }
                </ul>
             </div>);
     }
     
 }
     
-
 export default Weather;
--- src/WeatherBox.js
+++ src/WeatherBox.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import './WeatherBox.css';
 
-const WeatherBox = ( { children, weather } ) => {
-
+const WeatherBox = ( { children, item } ) => {
+    const [weather, setWeather] = useState(item);
+    
+    console.log(weather);
     return (
         <div className="weather-box-container">
-            <h3 className="weather-box-sol">Sol {children}</h3>
+            <h3 className="weather-box-sol">Sol {weather[0]}</h3>
+            <div></div>
+            
         </div>
         )
 
