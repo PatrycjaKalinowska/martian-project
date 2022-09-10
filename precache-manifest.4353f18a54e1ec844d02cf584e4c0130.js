f6a3167103ea9bc78f9ce0ce5c7af13c6424006f
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-09-10 17:06:09 +0200

    Updates

---
+++ precache-manifest.4353f18a54e1ec844d02cf584e4c0130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efa9f1ac919c28bbbed4",
+    "url": "/martian-project/static/css/main.09b0a321.chunk.css"
+  },
+  {
+    "revision": "efa9f1ac919c28bbbed4",
+    "url": "/martian-project/static/js/main.efa9f1ac.chunk.js"
+  },
+  {
+    "revision": "ad1f0438290a5c7124b7",
+    "url": "/martian-project/static/js/1.ad1f0438.chunk.js"
+  },
+  {
+    "revision": "46a8801944da02837b3b",
+    "url": "/martian-project/static/js/runtime~main.46a88019.js"
+  },
+  {
+    "revision": "28f1fa561fd56819c8d88bb6a117e627",
+    "url": "/martian-project/index.html"
+  }
+];
