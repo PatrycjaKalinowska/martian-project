78fb5e6540962876109a8009c29e10858974a67e
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-09-10 16:03:26 +0200

    fixed RWD for Gallery page

--- src/pages/Gallery/components/UserInput.js
+++ src/pages/Gallery/components/UserInput.js
@@ -10,16 +10,16 @@ const UserInput = ({ rover, setRover, day, setDay, month, setMonth, year, setYea
     const getInfo = () => {
         switch (rover) {
             case 'perseverance':
-                setInfo('Rover landed on Mars on February 18, 2021. Active until now.');
+                setInfo('(Rover landed on Mars on February 18, 2021. Active until now.)');
                 break;
             case 'curiocity':
-                setInfo('Rover landed on Mars on August 6, 2012. Active until now.');
+                setInfo('(Rover landed on Mars on August 6, 2012. Active until now.)');
                 break;
             case 'opportunity':
-                setInfo('Rover landed on Mars on January 25, 2004. Mission completed on February 13, 2019.');
+                setInfo('(Rover landed on Mars on January 25, 2004. Mission completed on February 13, 2019.)');
                 break;
             case 'spirit':
-                setInfo('Rover landed on Mars on January 4, 2004. Mission completed on May 25, 2011.');
+                setInfo('(Rover landed on Mars on January 4, 2004. Mission completed on May 25, 2011.)');
                 break;
         }
     }
@@ -31,7 +31,7 @@ const UserInput = ({ rover, setRover, day, setDay, month, setMonth, year, setYea
             newOption.innerHTML = i;
             selectYear.current.options.add(newOption);
         }
-        
+
     }
 
     const populateMonthOptions = () => {
@@ -45,24 +45,24 @@ const UserInput = ({ rover, setRover, day, setDay, month, setMonth, year, setYea
 
     const populateDayOptions = () => {
         if (month == 4 || month == 6 || month == 9 || month == 11) {
-            selectDay.current.innerHTML="";
+            selectDay.current.innerHTML = "";
             for (let i = 1; i <= 30; i++) {
                 const newOption = document.createElement("option");
                 newOption.value = i;
                 newOption.innerHTML = i;
                 selectDay.current.options.add(newOption);
             }
-        
+
         } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
-            selectDay.current.innerHTML="";
+            selectDay.current.innerHTML = "";
             for (let i = 1; i <= 31; i++) {
                 const newOption = document.createElement("option");
                 newOption.value = i;
                 newOption.innerHTML = i;
                 selectDay.current.options.add(newOption);
             }
-        } else if (month == 2 && year%4==0) {
-            selectDay.current.innerHTML="";
+        } else if (month == 2 && year % 4 == 0) {
+            selectDay.current.innerHTML = "";
             for (let i = 1; i <= 29; i++) {
                 const newOption = document.createElement("option");
                 newOption.value = i;
@@ -70,7 +70,7 @@ const UserInput = ({ rover, setRover, day, setDay, month, setMonth, year, setYea
                 selectDay.current.options.add(newOption);
             }
         } else {
-            selectDay.current.innerHTML="";
+            selectDay.current.innerHTML = "";
             for (let i = 1; i <= 28; i++) {
                 const newOption = document.createElement("option");
                 newOption.value = i;
@@ -80,12 +80,12 @@ const UserInput = ({ rover, setRover, day, setDay, month, setMonth, year, setYea
         }
     }
 
-    useEffect(() => {getInfo()}, [, rover]);
+    useEffect(() => { getInfo() }, [, rover]);
     useEffect(() => {
         populateYearOptions();
         populateMonthOptions();
     }, []);
-    useEffect(() => {populateDayOptions();}, [ , day, month, year]);
+    useEffect(() => { populateDayOptions(); }, [, day, month, year]);
 
     return (
         <form>
@@ -100,28 +100,30 @@ const UserInput = ({ rover, setRover, day, setDay, month, setMonth, year, setYea
                     <option value="opportunity">Opportunity</option>
                     <option value="spirit">Spirit</option>
                 </select>
-                <p className="select-info">{info}</p>
             </div>
+            <p className="select-info">{info}</p>
             <div className="input-container-date-picker">
-                <label className="date-picker-label label">Pick a date:</label>
                 <div className="date-picker-container">
-                    <label className="year-label label">Year:</label>
-                    <select ref={selectYear} className="select-year select" value={year} onChange={e => {
-                        setYear(e.target.value);
-                    }}>
-                    </select>
-                    <label className="month-label label">Month:</label>
-                    <select ref={selectMonth} className="select-month select" value={month} onChange={e => {
-                        setMonth(e.target.value);
-                    }}>
-                    </select>
-                    <label className="day-label label">Day:</label>
-                    <select ref={selectDay} className="select-day select" value={day} onChange={e => {
-                        setDay(e.target.value);
-                    }}>
-                    </select>
-                    <p className="date-display">You picked date: {year}-{month}-{day}</p>
+                    <label className="date-picker-label label">Pick a date:</label>
+                    <div className="date-picker">
+                        <label className="year-label label">Year:</label>
+                        <select ref={selectYear} className="select-year select" value={year} onChange={e => {
+                            setYear(e.target.value);
+                        }}>
+                        </select>
+                        <label className="month-label label">Month:</label>
+                        <select ref={selectMonth} className="select-month select" value={month} onChange={e => {
+                            setMonth(e.target.value);
+                        }}>
+                        </select>
+                        <label className="day-label label">Day:</label>
+                        <select ref={selectDay} className="select-day select" value={day} onChange={e => {
+                            setDay(e.target.value);
+                        }}>
+                        </select>
+                    </div>
                 </div>
+                <p className="date-display">(You picked date: {year}-{month}-{day})</p>
 
             </div>
         </form>
