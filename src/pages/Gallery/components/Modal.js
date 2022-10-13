42e2a782c20fd3b5ad83db53ea2dc1deedb144cd
Patrycja Kalinowska <patrycja_09@wp.pl>
2022-10-13 21:59:34 +0200

    additional fixes to styling in modal component

--- src/pages/Gallery/components/Modal.js
+++ src/pages/Gallery/components/Modal.js
@@ -2,12 +2,12 @@ import React from "react";
 import './Modal.css';
 import { FaTimes } from 'react-icons/fa';
 
-const Modal = ( { selectedPhoto, setSelectedPhoto } ) => {
-    
+const Modal = ({ selectedPhoto, setSelectedPhoto }) => {
+
     const handleClick = (e) => {
         if (e.target.classList.contains('backdrop')) {
             setSelectedPhoto(null);
-        } 
+        }
     }
 
     const handleClose = () => {
@@ -18,17 +18,17 @@ const Modal = ( { selectedPhoto, setSelectedPhoto } ) => {
         <div className="lightbox">
             <FaTimes className='close-button' onClick={handleClose}></FaTimes>
             <div className='photo-container'>
-                
-                <img className="photo" src={selectedPhoto.img_src} alt="enlarged"/>
+                <img className="photo" src={selectedPhoto.img_src} alt="enlarged" />
             </div>
             <div className="photo-footer">
-                    <p><span>Sol:</span> {selectedPhoto.sol}</p>
-                    <p><span>Earth date:</span> {selectedPhoto.earth_date}</p>
-                    <p><span>Rover:</span> {selectedPhoto.rover.name}</p>
-                    <p><span>Camera:</span> {selectedPhoto.camera.full_name}</p>
+                <p><span>Sol:</span> {selectedPhoto.sol}</p>
+                <p><span>Earth date:</span> {selectedPhoto.earth_date}</p>
+                <p><span>Rover:</span> {selectedPhoto.rover.name}</p>
+                <p><span>Camera:</span> {selectedPhoto.camera.full_name}</p>
             </div>
+
         </div>
-    </div>        
+    </div>
     )
 }
 
